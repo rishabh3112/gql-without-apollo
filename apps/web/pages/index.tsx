02d95a6c8857ac89c29c4a443090f484107db722
Rishabh Chawla <rishabh31121999@gmail.com>
2024-02-08 19:58:09 +0530

    chore: minor update

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,27 +1,14 @@
 import { Authors } from "@repo/ui/authors/Authors";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "/api/graphql",
-  cache: new InMemoryCache(),
-});
 
 export default function Page(): JSX.Element {
   return (
-    <ApolloProvider client={client}>
-      <div className="h-screen w-full flex justify-center">
-        <div className="flex flex-col gap-2 w-3/4 p-5 m-5 border-black rounded">
-          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
-            Authors ✍🏽
-          </h1>
-          <Authors />
-        </div>
+    <div className="h-screen w-full flex justify-center">
+      <div className="flex flex-col gap-2 w-3/4 p-5 m-5 border-black rounded">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl">
+          Authors ✍🏽
+        </h1>
+        <Authors />
       </div>
-    </ApolloProvider>
+    </div>
   );
 }
